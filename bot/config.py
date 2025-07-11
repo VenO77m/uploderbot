c7b03763f8334d3fece80c0bbf2cdab9c95ecbbb
VenO77m <karbaarad@gmail.com>
2025-07-11 16:13:11 +0330

    config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "7709150963:AAH1vJf5RVDJiW2_qP08a3df1Zt-wCLh4Cw"
 API_ID = 20575470
 API_HASH = "dc7c00c84ef9316b0d665542ec739870"
 
-FORCED_CHANNELS = ["gorouhedarsi", "femboykadeh", "bayeganygroup"]
+FORCED_CHANNELS = ["gorouhedarsi", "femboykadeh", "chanel_darsii"]
 OWNER_USERNAME = "Pooyaspd"
 WELCOME_TEXT = "سلام دانش‌آموز گلم خوش آمدی 💚"
 FILE_LIFETIME_SECONDS = 30
